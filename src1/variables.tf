0fd22461dc1089ba364e5dd39c65d9d29d992370
ifebres <hovard777@gmail.com>
2024-05-16 01:30:39 +0300

    init

---
+++ src1/variables.tf
@@ -0,0 +1,15 @@
+variable "iso_id" {
+  default = "fd89n8278rhueakslujo"
+}
+variable "yandex_zone" {
+  default = "ru-central1-a"
+}
+variable "user" {
+  default = "ifebres"
+}
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+variable "private_key_path" {
+  default = "~/.ssh/id_rsa"
+}
